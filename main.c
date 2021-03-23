776c0b44b42427b50ae715d2cd2b79815d76cce3
apambrose <64672545+apambrose@users.noreply.github.com>
2021-03-23 16:04:12 -0700

    bruh bruh gang gang Ggang

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Austin Ambrose
+ *
+ * Created on March 23, 2021, 4:01 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
